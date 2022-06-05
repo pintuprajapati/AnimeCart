a8b659c9c228da02a015ea86332aa7a43a2a3e74
Pintu <pintu1481999@gmail.com>
2022-06-05 18:42:03 +0530

    change in settings.py

--- animecart/settings.py
+++ animecart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*a-2@b3$@g*3f4y&*i9a*=4raj$wjf95r*usjro0n6-mhb#kmt'
+SECRET_KEY = '123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
