991064da394b04db9f2e25903549055309c4e3c4
Pintu <pintu1481999@gmail.com>
2022-06-05 22:01:00 +0530

    added staticfiles_storage (whitenoise)

--- animecart/settings.py
+++ animecart/settings.py
@@ -138,6 +138,8 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
